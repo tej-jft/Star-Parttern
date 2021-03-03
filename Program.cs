eb2906eea27ad8bfcb3f1ffe7dadb4f9b249bc55
tej-jft <tej.prakash@jellyfishtechnologies.com>
2021-03-03 16:50:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Star_Parttern
+{
+    class Program
+    {
+        static void partern(int n)
+        {
+            if (n <= 1)
+            {
+                Console.WriteLine("*");
+                return;
+            }
+            Line(n);
+            Console.WriteLine("");
+            int p = n - 1;
+            partern(p);
+            return;
+        }
+        static void Line(int l)
+        {
+            if (l > 0)
+            {
+                Console.Write("*");
+                int t = l - 1;
+                Line(t);
+            }
+            return;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            partern(n);
+            
+        }
+    }
+}
